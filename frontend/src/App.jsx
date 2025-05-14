49052823c11ff32659c997e1c801606eebd74633
Roseline <rdami007@fiu.edu>
2025-05-14 01:27:48 -0400

    testing setup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import LandingPage from './components/LandingPage'
 import OptionPage from './components/OptionPage'
+import SetupPage from './components/SetupPage'
 import { testCards } from './constants/TextConstants'
 
 function App() {
 
   return (
     <div>
-      <LandingPage/>
+      <SetupPage/>
     </div>
   )
 }

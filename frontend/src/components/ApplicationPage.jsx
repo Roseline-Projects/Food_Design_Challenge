40e8fda12eff85fce5c8a0e92dc94de80985a962
Roseline <rdami007@fiu.edu>
2025-05-28 14:48:04 -0400

    forgot to push the responsive part

--- frontend/src/components/ApplicationPage.jsx
+++ frontend/src/components/ApplicationPage.jsx
@@ -10,8 +10,8 @@ const Header = () => {
   return(
     <div className="pt-12 bg-orange-300">
       <div className="relative bg-orange-300 pb-6 lg:pb-0">
-        <h1 className="lg:absolute top-18 left-0 pl-18 px-12 text-7xl font-bold">Let's Get Started</h1>
-        <svg className="hidden lg:block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319"><path fill="#fff" fill-opacity="1" d="M0,288L80,282.7C160,277,320,267,480,240C640,213,800,171,960,160C1120,149,1280,171,1360,181.3L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+        <h1 className="lg:absolute top-18 left-0 pl-18 md:px-12 text-4xl md:text-7xl font-bold">Let's Get Started</h1>
+        <svg className="hidden lg:block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319"><path fill="#fff" fillOpacity="1" d="M0,288L80,282.7C160,277,320,267,480,240C640,213,800,171,960,160C1120,149,1280,171,1360,181.3L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
         {/* <svg className="block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319"><path fill="#fff" fill-opacity="1" d="M0,256L48,256C96,256,192,256,288,245.3C384,235,480,213,576,213.3C672,213,768,235,864,256C960,277,1056,299,1152,282.7C1248,267,1344,213,1392,186.7L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg> */}
       </div>
     </div>
@@ -24,8 +24,8 @@ const DuringSection = () => {
   return(
     <section className="bg-white shadow-sm p-12 my-14 rounded-2xl">
       <h3 className="text-2xl sm:text-4xl text-dark-green font-bold text-center mb-10">During Your Application</h3>
-      <div className="bg-green-100 rounded-2xl px-10 py-16 flex items-center gap-10 shadow-sm">
-        <div className="size-[54px] rounded-full">
+      <div className="bg-green-100 rounded-2xl px-10 py-16 md:flex items-center gap-10 shadow-sm">
+        <div className="size-[54px] rounded-full mb-10 md:mb-0">
           <IconContext.Provider value={{color: '#ED8530', size:'5rem'}}>
             <LuSparkles />
           </IconContext.Provider>
@@ -48,9 +48,9 @@ const DuringSection = () => {
 //Displays the numbered cards at the After App section
 const StepCard = ({header, para, number}) => {
   return (
-    <div className="grid grid-cols-4 items-center p-1 gap-10">
+    <div className="grid md:grid-cols-4 items-center p-1 md:gap-10">
       <div>
-        <h4 className="text-8xl text-right text-orange mx-6">{number}</h4>
+        <h4 className="text-8xl text-right text-orange md:mx-6">{number}</h4>
       </div>
       <div className="col-span-3 px-2">
         <h5 className="text-2xl text-dark-green mb-3">{header}</h5>
@@ -66,7 +66,6 @@ const AfterSection = () => {
   return(
     <section className="bg-white shadow-sm p-12 my-14 rounded-2xl">
       <h3 className="text-2xl sm:text-4xl text-dark-green font-bold text-center mb-10">After You Submit</h3>
-      {console.log(Object.entries(stepCardInfo))}
       <div>
         <ul className="space-y-10">
           {
@@ -230,7 +229,7 @@ const SnapInfoPage = () => {
 
             <DuringSection />
             <AfterSection />
-            
+
           </div>
         </div>
       </div>

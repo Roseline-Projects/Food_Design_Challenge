b73f1d9a69e82f774df9e4ad0bc136a49d0777bf
Roseline <rdami007@fiu.edu>
2025-05-28 14:49:18 -0400

    the text constants

---
+++ frontend/src/constants/TextConstants.js
@@ -0,0 +1,32 @@
+export const landingTextConstants = {
+    nearbyStores: {
+      header: 'Find Nearby Stores',
+      para: "If you're getting benefits but you don't know where to use them, you can locate SNAP-eligible near you with our locator tool.",
+      para2: "Our tool helps you find food sources near you, from grocery stores to farmer's markets and everything in-between, with just the click of a button.",
+      linkText: 'Explore'
+    },
+    apply: {
+      header: 'Start Saving Today',
+      para: "How do you apply? You'll need proof of identification, income, and residency. Don't worry, our in-app Agent will help you through your SNAP Application and answer any tough questions on-demand.",
+      para2: 'Ready to send an application and give your budget a boost? Apply for SNAP Today.',
+      linkText: 'Apply Now'
+    }
+  }
+
+export const stepCardInfo = {
+  step1: {
+    number: '1.',
+    heading: 'Receiving Your Benefits',
+    para: 'You may wait up to 30 days to hear back about your application. You can check its status on your application dashboard.',
+  },
+  step2: {
+    number: '2.',
+    heading: 'Using Your Benefits',
+    para: 'You can use your new EBT card and access aid to purchase fruits, vegetables, meats, dairy products, and bread.',
+  },
+  step3: {
+    number: '3.',
+    heading: 'Finding SNAP-Eligible Stores',
+    para: 'Find stores that will accept your benefits. Our site offers a comprehensive locator tool to help you find SNAP-eligible stores anywhere in Florida at your convienence.',
+  },
+}
